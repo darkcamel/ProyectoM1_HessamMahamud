504841d127a9e6c763f1ab64be6f360f2aca91bb
darkcamel <h_mahamud_b@hotmail.com>
2026-07-15 14:32:25 -0500

    chore: añade interactividad botón

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ const sizeSelec = document.getElementById('palette-size');
 const paletteContainer = document.getElementById('palette-container');
 
 //=< evento click >==
+generateButton.addEventListener('Click', () => {
+    const size = parseInt(sizeSelect.value, 10);
+    renderEmptyPalette(size);
+});
+
