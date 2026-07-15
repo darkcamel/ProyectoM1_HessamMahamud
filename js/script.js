32c88a12b4787d3576e4616748c1a59652c9e024
darkcamel <h_mahamud_b@hotmail.com>
2026-07-15 14:26:39 -0500

    chore: añade priemeras declaraciones js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//=< elementos clave >==
+const generateButton = document.getElementById('generate-palette');
+const sizeSelec = document.getElementById('palette-size');
+const paletteContainer = document.getElementById('palette-container');
+
+//=< evento click >==
